9e28f466ac31ead486962603b3467f6c82134faa
pafr23wt <148805767+pafr23wt@users.noreply.github.com>
2023-12-07 11:53:53 +0100

    upd javashript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 //taking user back to top
 const toTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
 
+
+//Effect to make the pictures larger while hovering with the mouse
+
+window.addEventListener('load', function () {
+  // Function to add zoom effect on image hover
+  function addZoomEffect() {
+    this.classList.add('zoomed');
+  }
+
+  // Function to remove zoom effect on image mouseout
+  function removeZoomEffect() {
+    this.classList.remove('zoomed');
+  }
+
+  document.querySelectorAll('.accessories-pictures img').forEach(function (img) {
+    img.addEventListener('mouseover', addZoomEffect);
+    img.addEventListener('mouseout', removeZoomEffect);
+  });
+});
